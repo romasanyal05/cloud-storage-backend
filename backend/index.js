da4fbde82439f821deb16ed223b47f945d725ecc
Garima Bhushan <romasanyal05@gmail.com>
2026-01-20 18:48:09 +0530

    update cors origin

--- backend/index.js
+++ backend/index.js
@@ -16,22 +16,19 @@ const app = express();
 // ✅ CORS CODE HERE
 const allowedOrigins = [
   "https://cloud-storage-frontend-sigma.vercel.app",
+  "https://cloud-storage-frontend-3d89mbtha-labmantix.vercel.app", //
   "http://localhost:5173",
 ];
 
-app.use(
-  cors({
+app.use(cors({
     origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-
-      return callback(null, true);
-    },
+     // Agar request bina origin ke ho (jaise mobile apps) ya origin list mein ho
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS policy error: Origin not allowed"));
+    }
+  }, 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
