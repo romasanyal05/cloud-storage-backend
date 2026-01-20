2251e960783e74565a80c95752478c63e84246be
Garima Bhushan <romasanyal05@gmail.com>
2026-01-20 18:20:53 +0530

    update cors origin

--- backend/index.js
+++ backend/index.js
@@ -12,16 +12,31 @@ const { supabaseAdmin, supabaseAuth } = require("./supabase");
 const supabase = supabaseAuth;
 const authMiddleware = require("./middleware/authMiddleware");
 const paymentRoutes = require("./routes/paymentRoutes");
-
 const app = express();
+// ✅ CORS CODE HERE
+const allowedOrigins = [
+  "https://cloud-storage-frontend-sigma.vercel.app",
+  "http://localhost:5173",
+];
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
 
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+
+      return callback(null, true);
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.options("*", cors());
 
 app.use(express.json());
